d7decaf3fb968543ea74398f5a544a036f401fe4
p1ckLL <86033747+p1ckLL@users.noreply.github.com>
2022-05-25 18:10:08 -0700

    big patch
    Character limit
    You cannot receive messages unless logged in
    You will not be counted as a user until logged in

--- static/scripts/mainScript.js
+++ static/scripts/mainScript.js
@@ -2,7 +2,7 @@ let windowPath = window.location.pathname
 let roomName = windowPath.startsWith('/rooms/') ? windowPath.split('/rooms/')[1] : null
 console.log(roomName)
 
-const socket = io('/', {
+let socket = io('/', {
   query : {
     room : roomName
   }
@@ -25,42 +25,42 @@ pwdForm.addEventListener('submit', (e) => {
   socket.on("pwd success", () => {
     pwdForm.style.display = "none"
     msgForm.style.display = "flex"
+
+    msgForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (input.value && author.value.trim() != '') {
+        socket.emit('message', [input.value, author.value]);
+        input.value = '';
+        sendButton.disabled = true
+        setTimeout(() => {
+          sendButton.disabled = false
+        }, 700)
+      } else {
+        author.value = 'stop using an invisible name nerd'
+      }
+    });
+    
+    socket.on("failed password", () => {
+      window.location = "/deadend"
+    })
+    
+    socket.on('message', (msgObj) => {
+      var item = document.createElement('li');
+      item.id = "sentMsgText"
+      item.textContent = msgObj.author + ": " + msgObj.message;
+      if (msgObj.isOfficial == true) {
+        item.style.color = 'red'
+      }
+      messages.appendChild(item);
+      item.scrollIntoView(false)
+    });
+    
+    socket.on("user-count update", (userCount) => {
+      userCountUI.innerHTML = `${userCount} users online`
+    })
+    
+    socket.on("refresh", () => {
+      window.location.reload()
+    })
   })
-})
-
-msgForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (input.value && author.value.trim() != '') {
-    socket.emit('message', [input.value, author.value]);
-    input.value = '';
-    sendButton.disabled = true
-    setTimeout(() => {
-      sendButton.disabled = false
-    }, 700)
-  } else {
-    author.value = 'stop using an invisible name nerd'
-  }
-});
-
-socket.on("failed password", () => {
-  window.location = "/deadend"
-})
-
-socket.on('message', (msgObj) => {
-  var item = document.createElement('li');
-  item.id = "sentMsgText"
-  item.textContent = msgObj.author + ": " + msgObj.message;
-  if (msgObj.isOfficial == true) {
-    item.style.color = 'red'
-  }
-  messages.appendChild(item);
-  item.scrollIntoView(false)
-});
-
-socket.on("user-count update", (userCount) => {
-  userCountUI.innerHTML = `${userCount} users online`
-})
-
-socket.on("refresh", () => {
-  window.location.reload()
 })
