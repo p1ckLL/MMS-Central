923112e805d006bdc7b97ac40548ffba86098266
p1ckLL <86033747+p1ckLL@users.noreply.github.com>
2022-05-30 12:05:59 -0700

    Revert "Merge branch 'main' into working"
    This reverts commit 52e0f9e7b67bbe1089bee103df8ac1052500d28a, reversing
    changes made to 01aad046bec00e01775d2a77e78389d730c10bb3.

--- static/scripts/mainScript.js
+++ static/scripts/mainScript.js
@@ -2,7 +2,7 @@ let windowPath = window.location.pathname
 let roomName = windowPath.startsWith('/rooms/') ? windowPath.split('/rooms/')[1] : null
 console.log(roomName)
 
-let socket = io('/', {
+const socket = io('/', {
   query : {
     room : roomName
   }
@@ -10,10 +10,11 @@ let socket = io('/', {
 
 const messages = document.getElementById('messages');
 const msgForm = document.getElementById('msgForm');
+const pwdForm = document.getElementById('pwdForm');
 const input = document.getElementById('msgInput');
 const author = document.getElementById('authorInput')
 const sendButton = document.getElementById('sendButton')
-
+const pwd = document.getElementById('pwd')
 const userCountUI = document.getElementById("usercount")
 
 const charLimit = 570
@@ -57,3 +58,11 @@ socket.on('message', (msgObj) => {
   messages.appendChild(item);
   item.scrollIntoView(false)
 });
+
+socket.on("user-count update", (userCount) => {
+  userCountUI.innerHTML = `${userCount} users online`
+})
+
+socket.on("refresh", () => {
+  window.location.reload()
+})
